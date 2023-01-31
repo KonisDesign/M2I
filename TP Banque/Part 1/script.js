4e17042c339487431ac1fef16ce008d56f89c4cd
KonisDesign <lhacktu@gmail.com>
2023-01-31 13:31:00 +0100

    Update script.js

--- TP Banque/Part 1/script.js
+++ TP Banque/Part 1/script.js
@@ -1,9 +1,19 @@
 function agios() {
     let decouvert = 0;
     while (decouvert > 2000 || decouvert < 100) {
-        decouvert = Number(prompt("Entrez le montant de votre découvert"));
+        decouvert = Number(prompt("Entrez le montant de votre découvert autorisé"));
+        if (decouvert == 0) {
+            break;
+        }
     }
-    let day = Number(prompt("Entrez le nombre de jour"));
-    let result = decouvert * day * 0.1 / 365;
-    document.getElementById("show").innerHTML = "Agios: " + result.toFixed(2);
+    if (decouvert == 0) {
+        document.getElementById("show").innerHTML = "Découvert non autorisé, pas d'agios";
+    } else {
+        let day = 0;
+        while (day <= 0 || day > 365) {
+            day = Number(prompt("Entrez le nombre de jour"));
+        }
+        let result = decouvert * day * 0.1 / 365;
+        document.getElementById("show").innerHTML = "Agios: " + result.toFixed(2);
+    }
 }
