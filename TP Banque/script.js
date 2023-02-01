116ed9bfa6b7d9b6606f60f7a11d3fda73b2a3b3
KonisDesign <lhacktu@gmail.com>
2023-02-01 10:23:52 +0100

    Final

--- TP Banque/script.js
+++ TP Banque/script.js
@@ -1,23 +1,30 @@
 let decouvert = 0;
 let deposit = 0;
 let sold = 0;
+let tasks = [];
 
 function openaccount() {
     let undecouvert = prompt("Voulez-vous avoir un découvert ?");
     if (undecouvert == "y" || undecouvert == "Y") {
+        tasks.push("Ajout d'un découvert");
         while (decouvert > 2000 || decouvert < 100) {
             decouvert = Number(prompt("Entrez le montant de votre découvert autorisé"));
         }
         document.getElementById("overdraft").innerHTML = "Découvert (€): " + decouvert;
+        tasks.push("Montant du découvert autorisé: " + decouvert + "€");
     } else {
         document.getElementById("overdraft").innerHTML = "Découvert: non autorisé";
+        tasks.push("Pas de découvert autorisé");
     }
     while (sold < 500) {
         sold = Number(prompt("Saisissez le dépot"));
     }
+    tasks.push("Dépot de " + sold + "€");
     document.getElementById("usersold").innerHTML = "Solde (€): " + sold;
     document.getElementById("open-account-button").style.display = "none";
     document.getElementById("withdraw-button").style.display = "block";
+    document.getElementById("tasks-container").style.display = "flex";
+    readtab();
 }
 
 function withdraw() {
@@ -27,6 +34,8 @@ function withdraw() {
     if (check >= 0) {
         sold = sold - withdraw;
         document.getElementById("usersold").innerHTML = "Solde (€): " + sold;
+        tasks.push("Retrait de " + withdraw + "€");
+        tasks.push("Solde restant: " + sold + "€");
     }
     else {
         document.getElementById("show").innerHTML = "Solde insuffisant";
@@ -34,7 +43,9 @@ function withdraw() {
 
     if (sold < 0) {
         document.getElementById("agios-button").style.display = "block";
+        tasks.push("Vous êtes à découvert");
     }
+    readtab();
 }
 
 function agios() {
@@ -44,5 +55,16 @@ function agios() {
         day = Number(prompt("Entrez le nombre de jour"));
     }
     let result = (sold * -1) * day * 0.1 / 365;
+    tasks.push("Sur " + day + " Jour(s) vous avez " + result.toFixed(2) + "€ d'intérêts");
     document.getElementById("show").innerHTML = "Agios: " + result.toFixed(2);
+    readtab();
+}
+
+function readtab() {
+    let mytask = "";
+    for (let i = 0; i < tasks.length; i++) {
+        mytask = mytask + tasks[i] + "<br>";
+        document.getElementById("tasks-show").innerHTML = mytask;
+    }
+    console.log(tasks);
 }
