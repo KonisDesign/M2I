43da491a4aeb4417dd7bd28937c632c7889d772a
KonisDesign <lhacktu@gmail.com>
2023-02-02 13:46:53 +0100

    Classics

--- Exercice/script.js
+++ Exercice/script.js
@@ -1,15 +1,4 @@
-function impot() {
-    let age = prompt("Quel âge avez-vous ?");
-    let genre = prompt("Êtes-vous un homme ? Une femme ? (h/f)");
-    let result = "";
-    if (genre == "h" && age >= 20) {
-        result = "Vous payez des impôts";
-    } else if (genre == "f" && age >= 18 && age <= 35) {
-        result = "Vous payez des impôts";
-    }
-    else {
-        result = "Vous ne payez pas d'impôts";
-    }
-    document.getElementById("show").innerHTML = result;
-}
+import impot from "./classes";
 
+const unnomquetuveux = new impot;
+document.getElementById("impotbutton").addEventListener("click", unnomquetuveux.impot);
