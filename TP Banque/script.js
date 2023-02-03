8e18c4d503fc09dd0d5cc827d90bb0ff7b292ab9
KonisDesign <lhacktu@gmail.com>
2023-02-03 14:17:37 +0100

    Design

--- TP Banque/script.js
+++ TP Banque/script.js
@@ -1,14 +1,41 @@
 let decouvert = 0;
-let deposit = 0;
 let sold = 0;
 let tasks = [];
+let nameuser = "";
+
+try {
+    decouvert = Number(localStorage.getItem("mydecouvert"));
+    sold = Number(localStorage.getItem("mysold"));
+    tasks = JSON.parse(localStorage.getItem("mytasks"));
+    nameuser = localStorage.getItem("myname");
+} catch {
+
+}
+
+if (sold != null && decouvert != null && tasks != null && nameuser != null) {
+    document.getElementById("usersold").innerHTML = "Solde (€): " + sold;
+    document.getElementById("overdraft").innerHTML = "Découvert (€): " + decouvert;
+    document.getElementById("title").innerHTML = "Bonjour " + nameuser;
+    document.getElementById("open-account-button").style.display = "none";
+    document.getElementById("withdraw-button").style.display = "block";
+    document.getElementById("deposit-button").style.display = "block";
+    document.getElementById("signout-button").style.display = "block";
+    document.getElementById("tasks-container").style.display = "flex";
+    readtab();
+} else {
+    decouvert = 0;
+    sold = 0;
+    tasks = [];
+}
 
 function openaccount() {
+    nameuser = prompt("Veuillez entrer votre nom et votre prénom");
     let undecouvert = prompt("Voulez-vous avoir un découvert ?");
     if (undecouvert == "y" || undecouvert == "Y") {
         tasks.push(getdate() + ": Ajout d'un découvert");
         while (decouvert > 2000 || decouvert < 100) {
             decouvert = Number(prompt("Entrez le montant de votre découvert autorisé"));
+            save("mydecouvert", decouvert);
         }
         document.getElementById("overdraft").innerHTML = "Découvert (€): " + decouvert;
         tasks.push(getdate() + ": Montant du découvert autorisé: " + decouvert + "€");
@@ -21,11 +48,15 @@ function openaccount() {
     }
     tasks.push(getdate() + ": Dépot de " + sold + "€");
     document.getElementById("usersold").innerHTML = "Solde (€): " + sold;
+    document.getElementById("title").innerHTML = "Bienvenue " + nameuser;
     document.getElementById("open-account-button").style.display = "none";
     document.getElementById("withdraw-button").style.display = "block";
     document.getElementById("deposit-button").style.display = "block";
+    document.getElementById("signout-button").style.display = "block";
     document.getElementById("tasks-container").style.display = "flex";
     readtab();
+    save("mysold", sold);
+    save("myname", nameuser);
 }
 
 function mydeposit() {
@@ -42,6 +73,7 @@ function mydeposit() {
         tasks.push(getdate() + ": Vous êtes à découvert");
     }
     readtab();
+    save("mysold", sold);
 }
 
 function withdraw() {
@@ -63,6 +95,7 @@ function withdraw() {
         tasks.push(getdate() + ": Vous êtes à découvert");
     }
     readtab();
+    save("mysold", sold);
 }
 
 function agios() {
@@ -84,7 +117,7 @@ function readtab() {
         document.getElementById("tasks-show").innerHTML = "<br>" + mytask;
     }
     document.getElementById("tasks-show").scrollTop = document.getElementById("tasks-show").scrollHeight;
-
+    localStorage.setItem("mytasks", JSON.stringify(tasks));
 }
 
 function getdate() {
@@ -94,4 +127,13 @@ function getdate() {
     let yyyy = today.getFullYear();
     today = "<b>" + dd + "/" + mm + "/" + yyyy + "</b>";
     return today;
+}
+
+function save(myid, myvalue) {
+    localStorage.setItem(myid, myvalue);
+}
+
+function signout() {
+    localStorage.clear();
+    location.reload();
 }
