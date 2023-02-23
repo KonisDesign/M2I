09faaa7b3fa0d37f0b4d84f38f4224ca66c18d6d
KonisDesign <lhacktu@gmail.com>
2023-02-23 16:57:57 +0100

    bug fix

--- TP Nombre mystère/script.js
+++ TP Nombre mystère/script.js
@@ -14,14 +14,15 @@ function check() {
     stroke++;
     document.getElementById("stroke").innerHTML = "Nombre de coups : " + stroke;
     let myanswer = document.getElementById('myinput').value;
+    document.getElementById("myinput").value = "";
     if (myanswer == mysternumber) {
         document.getElementById('first').innerHTML = "Bravo...! ! ! Vous avez trouvé en " + stroke + " coups!";
         document.getElementById('second').innerHTML = "Le nombre mystère était " + mysternumber;
         document.getElementById("check").disabled = true;
         document.getElementById("myinput").disabled = true;
-    } else if (myanswer < mysternumber) {
+    } else if (myanswer < mysternumber && myanswer > 0) {
         document.getElementById('first').innerHTML = "Le nombre mystère est plus grand que " + myanswer;
-    } else if (myanswer > mysternumber) {
+    } else if (myanswer > mysternumber && myanswer <= 50) {
         document.getElementById('first').innerHTML = "Le nombre mystère est plus petit que " + myanswer;
     } else {
         document.getElementById('first').innerHTML = "Veuillez écrire un nombre entre 1 et 50";
